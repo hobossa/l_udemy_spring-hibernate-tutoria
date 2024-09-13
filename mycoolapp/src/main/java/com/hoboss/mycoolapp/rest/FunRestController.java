e0b08ab7d5d1b307472359ad22a9f8fa7f1716b1
Hao Zhan <hoboss@gmail.com>
2024-09-12 23:48:22 -0700

    read application.properties. inject a value

--- mycoolapp/src/main/java/com/hoboss/mycoolapp/rest/FunRestController.java
+++ mycoolapp/src/main/java/com/hoboss/mycoolapp/rest/FunRestController.java
@@ -1,14 +1,18 @@
 package com.hoboss.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return appName + ": Hello World!";
     }
 }
