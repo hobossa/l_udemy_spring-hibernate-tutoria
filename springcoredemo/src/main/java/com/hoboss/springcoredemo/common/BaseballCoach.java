1296f27e7445df5887758503c4c45177f6c2d60b
Hao Zhan <hoboss@gmail.com>
2024-09-18 01:51:25 -0700

    @Lazy annotation so specify lazy initialization. only created when it is needed.

--- springcoredemo/src/main/java/com/hoboss/springcoredemo/common/BaseballCoach.java
+++ springcoredemo/src/main/java/com/hoboss/springcoredemo/common/BaseballCoach.java
@@ -1,9 +1,15 @@
 package com.hoboss.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class BaseballCoach implements Coach{
+    public BaseballCoach() {
+        System.out.println("BaseballCoach construction");
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes in batting practice.";
