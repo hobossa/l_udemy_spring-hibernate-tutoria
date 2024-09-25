2593a6415557b26ea41d896ab5a71157b3d67563
Hao Zhan <hoboss@gmail.com>
2024-09-25 00:57:56 -0700

    add bean lifecycle methods: Annotations: PostConstruct and PreDestroy

--- springcoredemo/src/main/java/com/hoboss/springcoredemo/common/CricketCoach.java
+++ springcoredemo/src/main/java/com/hoboss/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.hoboss.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -12,5 +14,15 @@ public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes!";
+    }
+
+    @PostConstruct
+    public void doStartupStuff() {
+        System.out.println("In doStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doCleanStuff() {
+        System.out.println("In doCleanStuff(): " + getClass().getSimpleName());
     }
 }
