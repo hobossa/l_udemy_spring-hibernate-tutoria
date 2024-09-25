2835fe7a3f5157945c0bef27b2eb1fa7ce550669
Hao Zhan <hoboss@gmail.com>
2024-09-25 00:53:29 -0700

    remove a injection of coach

--- springcoredemo/src/main/java/com/hoboss/springcoredemo/rest/DemoController.java
+++ springcoredemo/src/main/java/com/hoboss/springcoredemo/rest/DemoController.java
@@ -11,22 +11,12 @@ public class DemoController {
 
     private Coach myCoach;
 
-    private Coach anotherCoach;
 
     @Autowired
     public void setMyCoach(@Qualifier("cricketCoach") Coach myCoach) {
         this.myCoach = myCoach;
     }
 
-    @Autowired
-    public void setAnotherCoach(@Qualifier("cricketCoach") Coach anotherCoach) {
-        this.anotherCoach = anotherCoach;
-    }
-
-    @GetMapping("/check")
-    public String check() {
-        return "myCoach == anotherCoach:" + (this.myCoach == this.anotherCoach);
-    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
