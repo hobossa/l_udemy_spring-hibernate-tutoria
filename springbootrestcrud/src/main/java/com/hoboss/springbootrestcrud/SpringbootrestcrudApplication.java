3a32fe9f7ceeb6fe89506e4829cd9919d4bdb72c
Hao Zhan <hoboss@gmail.com>
2024-10-07 23:57:03 -0700

    init 04-01 spring boot rest crud

---
+++ springbootrestcrud/src/main/java/com/hoboss/springbootrestcrud/SpringbootrestcrudApplication.java
@@ -0,0 +1,13 @@
+package com.hoboss.springbootrestcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestcrudApplication.class, args);
+	}
+
+}
