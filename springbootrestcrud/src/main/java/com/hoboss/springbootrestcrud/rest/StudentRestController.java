449d51d23bdd1e59b894591669ebe9a37ca98d2b
Hao Zhan <hoboss@gmail.com>
2024-10-13 00:03:33 -0700

    Spring Boot REST Exception Handling demo

--- springbootrestcrud/src/main/java/com/hoboss/springbootrestcrud/rest/StudentRestController.java
+++ springbootrestcrud/src/main/java/com/hoboss/springbootrestcrud/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.hoboss.springbootrestcrud.rest;
 
 import com.hoboss.springbootrestcrud.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +29,20 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId < 0 || studentId >= students.size()) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleStudentNotFoundException(StudentNotFoundException exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
