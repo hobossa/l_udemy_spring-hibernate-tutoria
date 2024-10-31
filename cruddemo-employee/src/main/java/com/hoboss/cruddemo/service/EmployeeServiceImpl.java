4f54e0550c222d06d0adcf3d8f14b15bdb1ed376
Hao Zhan <hoboss@gmail.com>
2024-10-31 06:30:31 -0700

    Spring Boot REST: Spring Data JPA (JpaRepository)

--- cruddemo-employee/src/main/java/com/hoboss/cruddemo/service/EmployeeServiceImpl.java
+++ cruddemo-employee/src/main/java/com/hoboss/cruddemo/service/EmployeeServiceImpl.java
@@ -1,42 +1,48 @@
 package com.hoboss.cruddemo.service;
 
-import com.hoboss.cruddemo.dao.EmployeeDAO;
+import com.hoboss.cruddemo.dao.EmployeeRepository;
 import com.hoboss.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return employeeDAO.findById(id);
+        final Optional<Employee> res = employeeRepository.findById(id);
+        if (res.isPresent()) {
+            return res.get();
+        } else {
+            throw new RuntimeException("Cannot find employee id - " + id);
+        }
     }
 
     @Override
     @Transactional
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
     @Override
     @Transactional
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
