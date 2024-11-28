935c74b1803d60d11e0191aa349821801192b9cc
Hao Zhan <hoboss@gmail.com>
2024-11-28 00:05:14 -0800

    Spring Boot MVC Adding Data to Spring MVC Model

--- thymeleafdemo/src/main/java/com/hoboss/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/hoboss/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.hoboss.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,5 +18,13 @@ public class HelloWorldController {
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        String name = request.getParameter("studentName");
+        model.addAttribute("message", "Yo! " + name.toUpperCase());
+        return "helloworld";
+
     }
 }
