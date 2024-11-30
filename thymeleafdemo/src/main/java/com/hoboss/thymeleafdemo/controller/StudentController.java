ab50fc38ae474ec035d269b25541508b119d89a1
Hao Zhan <hoboss@gmail.com>
2024-11-30 00:51:07 -0800

    Spring Boot MVC. Form Data Binding. Read country list from application.properties.

--- thymeleafdemo/src/main/java/com/hoboss/thymeleafdemo/controller/StudentController.java
+++ thymeleafdemo/src/main/java/com/hoboss/thymeleafdemo/controller/StudentController.java
@@ -1,18 +1,26 @@
 package com.hoboss.thymeleafdemo.controller;
 
 import com.hoboss.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
 
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/showForm")
     public String showForm(Model model) {
         Student student = new Student();
         model.addAttribute("student", student);
+
+        model.addAttribute("countries", countries);
         return "student-form";
     }
 
