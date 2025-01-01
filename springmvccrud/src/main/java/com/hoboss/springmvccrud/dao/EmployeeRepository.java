a6b3df05f9b3c85fa873b5dace4ac4d51d023abb
Hao Zhan <hoboss@gmail.com>
2025-01-01 03:01:19 -0800

    07, spring mvc crud demo init

---
+++ springmvccrud/src/main/java/com/hoboss/springmvccrud/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.hoboss.springmvccrud.dao;
+
+import com.hoboss.springmvccrud.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
