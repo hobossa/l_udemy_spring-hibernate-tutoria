c9b2f44aff65f7d7922c417f90f86b7c77166313
Hao Zhan <hoboss@gmail.com>
2025-02-12 00:25:20 -0800

    spring mvc crud. 1. Update Employee

--- springmvccrud/src/main/java/com/hoboss/springmvccrud/controller/EmployeeController.java
+++ springmvccrud/src/main/java/com/hoboss/springmvccrud/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.hoboss.springmvccrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -48,5 +45,12 @@ public class EmployeeController {
 
         // use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        Employee employee = employeeService.findById(id);
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
     }
 }
