0b52b878f77d662021b7717f426eff59f3aeb51a
Hao Zhan <hoboss@gmail.com>
2025-02-12 00:39:34 -0800

    spring mvc crud. 1. Delete Employee

--- springmvccrud/src/main/java/com/hoboss/springmvccrud/controller/EmployeeController.java
+++ springmvccrud/src/main/java/com/hoboss/springmvccrud/controller/EmployeeController.java
@@ -53,4 +53,10 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
         return "employees/employee-form";
     }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id) {
+        employeeService.deleteById(id);
+        return "redirect:/employees/list";
+    }
 }
