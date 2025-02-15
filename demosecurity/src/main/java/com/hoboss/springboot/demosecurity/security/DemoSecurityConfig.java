24ac839d12c09ccd553f1a0be0ef77cc0c86f498
Hao Zhan <hoboss@gmail.com>
2025-02-15 00:28:37 -0800

    08 spring mvc security: Custom Login Form

--- demosecurity/src/main/java/com/hoboss/springboot/demosecurity/security/DemoSecurityConfig.java
+++ demosecurity/src/main/java/com/hoboss/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -3,9 +3,11 @@ package com.hoboss.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -28,5 +30,14 @@ public class DemoSecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                        configurer.anyRequest().authenticated())
+                .formLogin(form ->
+                        form.loginPage("/showLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+        return httpSecurity.build();
     }
 }
