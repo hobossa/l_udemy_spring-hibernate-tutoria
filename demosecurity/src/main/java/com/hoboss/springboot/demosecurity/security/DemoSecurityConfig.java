f4149d9041bc94238b9bb4b315598b2da5d98fd0
Hao Zhan <hoboss@gmail.com>
2025-02-17 23:53:54 -0800

    08 spring mvc security: Login out

--- demosecurity/src/main/java/com/hoboss/springboot/demosecurity/security/DemoSecurityConfig.java
+++ demosecurity/src/main/java/com/hoboss/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -4,6 +4,7 @@ package com.hoboss.springboot.demosecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -37,7 +38,8 @@ public class DemoSecurityConfig {
         httpSecurity.authorizeHttpRequests(configurer ->
                         configurer.anyRequest().authenticated())
                 .formLogin(form ->
-                        form.loginPage("/showLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll());
+                        form.loginPage("/showLoginPage").loginProcessingUrl("/authenticateTheUser").permitAll())
+                .logout(LogoutConfigurer::permitAll);
         return httpSecurity.build();
     }
 }
