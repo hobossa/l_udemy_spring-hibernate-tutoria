a6f0391f70e11bc66af2866e9ba46b0a98f80516
Hao Zhan <hoboss@gmail.com>
2025-02-23 02:42:13 -0800

    09 Jpa advanced mapping, init

---
+++ jpaadvancedmapping/src/main/java/com/hoboss/jpaadvancedmapping/JpaadvancedmappingsApplication.java
@@ -0,0 +1,13 @@
+package com.hoboss.jpaadvancedmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaadvancedmappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaadvancedmappingsApplication.class, args);
+	}
+
+}
