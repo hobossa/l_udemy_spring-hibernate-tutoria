7872c8b2e47fa3aeb0f701f744b5bcef44b7ca58
Hao Zhan <hoboss@gmail.com>
2025-02-24 02:12:09 -0800

    09 Jpa advanced mapping. one-to-one-bi. deleteInstructorDetail

--- jpaadvancedmapping/src/main/java/com/hoboss/jpaadvancedmapping/dao/AppDAOImpl.java
+++ jpaadvancedmapping/src/main/java/com/hoboss/jpaadvancedmapping/dao/AppDAOImpl.java
@@ -40,4 +40,11 @@ public class AppDAOImpl implements AppDAO{
         return this.entityManager.find(InstructorDetail.class, id);
     }
 
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int id) {
+        InstructorDetail instructorDetail = this.entityManager.find(InstructorDetail.class, id);
+        this.entityManager.remove(instructorDetail);
+    }
+
 }
